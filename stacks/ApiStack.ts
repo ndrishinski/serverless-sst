a83f63926be215598e9422de9c8a89db3682cf6c
Nick <nick@Nicks-MBP-52.lan>
2024-02-25 21:37:12 -0500

    added put request for update

--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -14,6 +14,8 @@ export function ApiStack({ stack }: StackContext) {
     routes: {
       "POST /notes": "packages/functions/src/create.main",
       "GET /notes/{id}": "packages/functions/src/get.main",
+      "GET /notes": "packages/functions/src/list.main",
+      "PUT /notes/{id}": "packages/functions/src/update.main",
     },
   });
 
