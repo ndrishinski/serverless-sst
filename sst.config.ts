427b164f685e0969ead68289c749018088ca42f9
ndrishinski <ndrishinski@gmail.com>
2024-03-02 10:19:03 -0500

    fixed auth issue from typo

--- sst.config.ts
+++ sst.config.ts
@@ -2,7 +2,7 @@ import { SSTConfig } from "sst";
 import { StorageStack } from "./stacks/StorageStack";
 import { ApiStack } from './stacks/ApiStack';
 import { AuthStack } from './stacks/AuthStack';
-import { FrontendStack } from "./stacks/FrontEndStack";
+import { FrontendStack } from "./stacks/FrontendStack";
 export default {
   config(_input) {
     return {
