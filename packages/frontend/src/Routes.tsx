62b73f45318457b3b8774bd78eda29732313806e
ndrishinski <ndrishinski@gmail.com>
2024-03-04 15:53:07 -0500

    added sign up validation

--- packages/frontend/src/Routes.tsx
+++ packages/frontend/src/Routes.tsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./containers/Home.tsx";
 import NotFound from './containers/NotFound.tsx'
 import Login from "./containers/Login.tsx";
-import Signup from './containers/SignUp.tsx'
+import Signup from './containers/Signup.tsx'
 
 export default function Links() {
   return (
