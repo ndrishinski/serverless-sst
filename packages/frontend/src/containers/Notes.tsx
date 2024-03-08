51e0a2de4af9e083aec91f4693b76300fa808815
ndrishinski <ndrishinski@gmail.com>
2024-03-08 15:37:39 -0500

    added logic to edit note

--- packages/frontend/src/containers/Notes.tsx
+++ packages/frontend/src/containers/Notes.tsx
@@ -8,6 +8,7 @@ import { onError } from "../lib/errorLib";
 import config from "../config";
 import { NoteType } from "../types/note";
 import LoaderButton from "../components/LoaderButton";
+import { s3Upload } from "../lib/awsLib";
 import "./Notes.css";
 
 export default function Notes() {
@@ -56,6 +57,12 @@ export default function Notes() {
     file.current = event.currentTarget.files[0];
   }
   
+  function saveNote(note: NoteType) {
+    return API.put("notes", `/notes/${id}`, {
+      body: note,
+    });
+  }
+  
   async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     let attachment;
   
@@ -71,6 +78,23 @@ export default function Notes() {
     }
   
     setIsLoading(true);
+  
+    try {
+      if (file.current) {
+        attachment = await s3Upload(file.current);
+      } else if (note && note.attachment) {
+        attachment = note.attachment;
+      }
+  
+      await saveNote({
+        content: content,
+        attachment: attachment,
+      });
+      nav("/");
+    } catch (e) {
+      onError(e);
+      setIsLoading(false);
+    }
   }
   
   async function handleDelete(event: React.FormEvent<HTMLFormElement>) {
